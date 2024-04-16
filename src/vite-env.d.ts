cf636a311724623e2705bab23123a636f9d67360
altyndai <azenisbekkyzy03@gmail.com>
2024-04-16 11:43:40 +0600

    commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,2 @@
 /// <reference types="vite/client" />
-declare module '*.vue' {
-    import { ComponentOptions } from 'vue';
-    const componentOptions: ComponentOptions;
-    export default componentOptions;
-}
+
