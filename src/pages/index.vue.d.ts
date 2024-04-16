cfa19334b8b4e6d337e980fb3b256c22a18b707b
altyndai <azenisbekkyzy03@gmail.com>
2024-04-16 11:38:36 +0600

    commit

--- src/pages/index.vue.d.ts
+++ src/pages/index.vue.d.ts
@@ -1,3 +1,5 @@
-import { defineComponent } from 'vue';
-const component: ReturnType<typeof defineComponent>;
-export default component;
+declare module '*.vue' {
+    import { ComponentOptions } from 'vue';
+    const componentOptions: ComponentOptions;
+    export default componentOptions;
+}
